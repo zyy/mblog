6d67ba1ea105aa894f99ef826fbc790f593873ba
Mccrea <wb-cdq330150@alibaba-inc.com>
2018-08-30 12:41:27 +0800

    修改注释

--- base/src/main/java/mblog/base/context/AppContext.java
+++ base/src/main/java/mblog/base/context/AppContext.java
@@ -17,41 +17,40 @@ import java.util.Map;
 
 /**
  * @author langhsu
- * 
  */
 @Component
 @Data
 public class AppContext {
-	
-	/*
-	 * 文件存储-根目录
-	 */
-	@Value("${site.store.root:/data/mblog/}")
-	String root = "/data/mblog";
-	
-	/*
-	 * 文件存储-原文件目录
-	 */
-	String origDir = "/store/orig";
-	
-	/*
-	 * 文件存储-压缩目录
-	 */
-	String thumbsDir = "/store/thumbs";
 
-	/*
-	 * 文件存储-头像目录
-	 */
-	String avaDir = "/store/ava";
-	
-	/*
-	 * 文件存储-临时文件目录
-	 */
-	String tempDir = "/store/temp";
+    /**
+     * 文件存储-根目录
+     */
+    @Value("${site.store.root:/data/mblog/}")
+    String root = "/data/mblog";
 
-	/*
-	 * 系统配置信息
-	 * - 在 StartupListener 类中加载
-	 */
-	public Map<String, String> config;
+    /**
+     * 文件存储-原文件目录
+     */
+    String origDir = "/store/orig";
+
+    /**
+     * 文件存储-压缩目录
+     */
+    String thumbsDir = "/store/thumbs";
+
+    /**
+     * 文件存储-头像目录
+     */
+    String avaDir = "/store/ava";
+
+    /**
+     * 文件存储-临时文件目录
+     */
+    String tempDir = "/store/temp";
+
+    /**
+     * 系统配置信息
+     * - 在 StartupListener 类中加载
+     */
+    public Map<String, String> config;
 }
