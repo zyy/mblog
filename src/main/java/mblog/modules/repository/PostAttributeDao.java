8f61ce22e57af7a7814450f889d2bd0c0707191c
xulang <xulang!@#$>
2019-01-04 16:41:35 +0800

    模块合并

---
+++ src/main/java/mblog/modules/repository/PostAttributeDao.java
@@ -0,0 +1,11 @@
+package mblog.modules.repository;
+
+import mblog.modules.entity.PostAttribute;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * Created by langhsu on 2017/9/27.
+ */
+public interface PostAttributeDao extends JpaRepository<PostAttribute, Long>, JpaSpecificationExecutor<PostAttribute> {
+}
---
+++ src/main/java/mblog/modules/repository/custom/FeedsDaoCustom.java
@@ -0,0 +1,16 @@
+package mblog.modules.repository.custom;
+
+import mblog.modules.data.FeedsVO;
+
+/**
+ * Created by langhsu on 2017/9/30.
+ */
+public interface FeedsDaoCustom {
+    /**
+     * 添加动态, 同时会分发给粉丝
+     *
+     * @param feeds
+     * @return
+     */
+    int batchAdd(FeedsVO feeds);
+}
