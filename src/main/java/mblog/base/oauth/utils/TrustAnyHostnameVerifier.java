b9b657ea388571c01380e4ff13f83bd394f7a660
xulang <xulang!@#$>
2019-01-04 16:40:31 +0800

    模块合并

---
+++ src/main/java/mblog/base/oauth/utils/TrustAnyHostnameVerifier.java
@@ -0,0 +1,14 @@
+package mblog.base.oauth.utils;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+
+class TrustAnyHostnameVerifier
+  implements HostnameVerifier
+{
+  public boolean verify(String hostname, SSLSession session)
+  {
+    return true;
+  }
+}
